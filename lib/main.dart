fc479a2f823e2016a1114181431f1a227e12d8e3
adminvns <shubham.editor@gmail.com>
2020-02-03 21:25:42 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+
+  runApp(
+    
+        Text(
+
+            "Hello Flutter",
+            textDirection: TextDirection.ltr
+
+        )
+  );
+}
